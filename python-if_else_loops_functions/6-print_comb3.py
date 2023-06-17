62ff185de1fea1c565f19ea7818db5b34175aa5c
FerHdzSchz <fernandohdz915@hotmail.com>
2023-06-17 13:55:58 -0500

    print comb3

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 for i in range(0,10):
-    for j in range(0,10):
+    for j in range(i+1,10):
         if i != j:
-            if (i == 9) and (j == 8):
+            if (i == 8) and (j == 9):
                 print(f"{i}{j}")
             else:
                 print(f"{i}{j}", end= ", ")
