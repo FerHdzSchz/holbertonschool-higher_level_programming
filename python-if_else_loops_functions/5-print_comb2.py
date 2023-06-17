64a495f7a1ea941e42af96294f53a2569fa7fdb7
FerHdzSchz <fernandohdz915@hotmail.com>
2023-06-17 13:41:50 -0500

    print 0-99

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-for i in range(0,10):
-    for j in range(0,10):
-        print(f"{i}{j}", end ="")
-        if i == 9 and j == 9:
-            continue
-        else:
-            print(", ", end="")
+for i in range(0,100):
+    if i != 99:
+        print(f"{i:2.d}", end ="")
+    else:
+        print(f"i", end="")
