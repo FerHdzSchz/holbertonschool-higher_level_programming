7a3938d5693eedbfd853d5d3e98115e995f54257
FerHdzSchz <fernandohdz915@hotmail.com>
2023-06-25 18:06:21 -0500

    get rid of list element

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    return [x[1] for x in enumerate(my_list) if x[0] != idx]
+    if (idx < 0) or (idx > len(my_list)):
+        return my_list
+
+    for x, y in enumerate(my_list):
+        if x == idx:
+            del my_list[idx]
+    return my_list
