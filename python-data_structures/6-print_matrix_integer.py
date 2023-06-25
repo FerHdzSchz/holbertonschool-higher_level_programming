626de4396dc5ce587d341d2109c81e2fdb01357a
FerHdzSchz <fernandohdz915@hotmail.com>
2023-06-25 15:40:19 -0500

    print matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
 
+    if matrix == [[]]:
+        print("\n")
     for i in range(len(matrix)):
-        if len(matrix) == 0:
-            print("\n")
         for j in range(len(matrix[i])):
             if j + 1 == len(matrix[i]):
                 print("{}".format(matrix[i][j]))
