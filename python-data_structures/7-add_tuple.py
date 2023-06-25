61b9129bd73307f4289bf67462fec9bbe6bdac40
FerHdzSchz <fernandohdz915@hotmail.com>
2023-06-25 17:00:17 -0500

    print tuple add

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    lena = len(tuple_a)
+    lenb = len(tuple_b)
+
+    if (lena == 0):
+        a_1, a_2 = 0, 0
+    if lenb == 0:
+        b_1, b_2 = 0, 0
+    if lena == 1:
+        a_1, a_2 = tuple_a[0], 0
+    if lenb == 1:
+        b_1, b_2 = tuple_b[0], 0
+    if lena == 2:
+        a_1, a_2 = tuple_a[0], tuple_a[1]
+    if lenb == 2:
+        b_1, b_2 = tuple_b[0], tuple_a[1]
+
+    return (a_1 + b_1, a_2 + b_2)
