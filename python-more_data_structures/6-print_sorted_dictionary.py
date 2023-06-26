a08ba70d54c53c5233a686e41c24fe08ec8474d9
FerHdzSchz <fernandohdz915@hotmail.com>
2023-06-25 21:22:51 -0500

    order keys

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    new_dict = {keyi: a_dictionary[keyi] for keyi in sorted(list(a_dictionary.keys()))}
+    new_dict = {k: a_dictionary[k] for k in sorted(list(a_dictionary.keys()))}
     for i in new_dict.items():
         print("{}: {}".format(i[0], i[1]))
