b1d4ac5931871d5e5cb18491b4e668b36dcb9848
FerHdzSchz <fernandohdz915@hotmail.com>
2023-06-25 20:59:20 -0500

    find differences

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -4,5 +4,4 @@ def only_diff_elements(set_1, set_2):
     same = [x for x in set_1 for y in set_2 if x == y]
     diff_list.append([x for x in set_1 if x not in same])
     diff_list.append([x for x in set_2 if x not in same])
-
     return [y for x in diff_list for y in x]
