c380503e23e6c8bd753c00f34b663f95cc9f451b
FerHdzSchz <fernandohdz915@hotmail.com>
2023-06-28 23:18:09 -0500

    adding safe print list

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,9 +3,9 @@ def safe_print_list(my_list=[], x=0):
     num_elem = 0
     for i in range(x):
         try:
-            print("{}".format(my_list[i]), end = "")
+            print("{}".format(my_list[i]), end="")
             num_elem += 1
-        except:
+        except IndexError:
             exit
     print()
     return num_elem
