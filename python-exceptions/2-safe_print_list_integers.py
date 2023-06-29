8519a97683337f054bedd0d5ef8e3c074b2712f7
FerHdzSchz <fernandohdz915@hotmail.com>
2023-06-29 18:51:37 -0500

    safe print list integers

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    num_elem = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            num_elem += 1
+        except (TypeError, NameError, ValueError, IndexError) as e:
+            continue
+    print()
+    return num_elem
