39cbea630974a16be386fc6b22a571567f3d8463
FerHdzSchz <fernandohdz915@hotmail.com>
2023-06-29 18:24:21 -0500

    exception print integer

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -4,7 +4,7 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         output = True
-    except Exception e:
+    except (TypeError, NameError) as e:
         pass
 
     return output
