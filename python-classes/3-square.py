8cd5b6f7599cc1109bac52b253c82bf4a3c99250
FerHdzSchz <fernandohdz915@hotmail.com>
2023-07-01 13:36:02 -0500

    square with area method

---
+++ python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Square class with error handling"""
+
+
+class Square:
+    """Base class + private attribute + type checker"""
+
+    def __init__(self, size=0):
+        """
+        Attr:
+            size: len of side
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Calculate area of a square
+        """
+        return self.__size**2
