e8434130ca5a5f18cd580c5457d543cd2d412968
FerHdzSchz <fernandohdz915@hotmail.com>
2023-07-02 16:06:07 -0500

    adding setter method

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -16,11 +16,19 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
+    @property
+    def size(self):
+        """"
+        Getter for size
+        """
+        return self.__size
+
+    @size.setter
     def size(self, value):
         """
         Setter for size
         """
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
