7e90973fad6c0f0ccb4e68b2391360102144cb74
FerHdzSchz <fernandohdz915@hotmail.com>
2023-07-06 22:45:50 -0500

    square

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Print Square
+"""
+
+
+def print_square(size):
+    """
+    Function to print a square with #
+
+    Params:
+    -------
+
+    size: integer
+    """
+
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    if (type(size) is not float) & (size < 0):
+        raise TypeError("size must be an integer")
+
+    for i in range(size):
+        print("{}".format("#"*size))
