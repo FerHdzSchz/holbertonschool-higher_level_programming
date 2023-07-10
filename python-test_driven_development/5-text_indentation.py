f3002005e99e2daf8848052efd6d1d51eac8a4ad
FerHdzSchz <fernandohdz915@hotmail.com>
2023-07-09 21:53:44 -0500

    text indentation

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -6,17 +6,13 @@ def text_indentation(text):
     """
     function that prints a text with 2 new lines after each of these characters: ., ? and :
     """
-
     if type(text) is not str:
         raise TypeError("text must be a string")
 
-    s = text[:]
-
-    for d in ".?:":
-        list_text = s.split(d)
-        s = ""
-        for i in list_text:
-            i = i.strip(" ")
-            s = i + d if s is "" else s + "\n\n" + i + d
-
-    print(s[:-3], end="")
+    line = ""
+    for c in range(len(text)):
+        line += text[c]
+        if text[c] in ".?:":
+            print((line + '\n').lstrip(' '))
+            line = ""
+    print(line.lstrip(' '), end='')
