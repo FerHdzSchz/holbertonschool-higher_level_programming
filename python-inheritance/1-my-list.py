210ea984c979ef3ca4ecbea52f002d4e37cb00b8
FerHdzSchz <fernandohdz915@hotmail.com>
2023-07-15 11:03:09 -0500

    adding class list inheritance

---
+++ python-inheritance/1-my-list.py
@@ -0,0 +1,10 @@
+class MyList(list):
+    """
+    Class that inherits from list
+    """
+
+    def print_sorted(self):
+        """
+        return sorted list
+        """
+        return sorted(self)
