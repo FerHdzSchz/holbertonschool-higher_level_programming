f4850c0acc83d4a0d032b50fb21a8903e49dc62a
FerHdzSchz <fernandohdz915@hotmail.com>
2023-07-15 21:24:07 -0500

    base geometry 2

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Empty class
+"""
+
+
+class BaseGeometry():
+    """
+    Implement area method with Exception
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+
